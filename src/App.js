8de3017ffb34194b4aac0a47c07ee52721684433
Hayato Watanabe <solideye@me.com>
2021-10-13 19:31:08 +0900

    update paragraph

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          お腹すいた😵‍💫
+          お腹すいた😵‍💫😵‍💫😵‍💫
         </p>
         <a
           className="App-link"
