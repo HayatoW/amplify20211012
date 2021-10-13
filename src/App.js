a0587c4fadc3a0248164ab633ac2cb5b0da7a5c1
Hayato Watanabe <solideye@me.com>
2021-10-13 20:21:05 +0900

    update paragraph

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          夕飯はカレー😋
+          Slack Attachments 検証中
         </p>
         <a
           className="App-link"
